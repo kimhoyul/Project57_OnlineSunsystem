0a50ff2d508651c7f162683d39b63d8dfec9a036
junios-study <junios@naver.com>
2025-12-31 15:44:56 +0900

    세션이름 맵이름 업데이트

--- Source/Project57/Session/SessionItemWidget.cpp
+++ Source/Project57/Session/SessionItemWidget.cpp
@@ -13,7 +13,20 @@ void USessionItemWidget::SetInfo(FBlueprintSessionResult InSessionResult)
 
 void USessionItemWidget::RefreshUI()
 {
-	Txt_SessionName->SetText(FText());
-	Txt_MapName->SetText(FText());
-	Txt_PlayerCount->SetText(FText());
+	FString SessionName;
+	Result.OnlineResult.Session.SessionSettings.Get(FName("SESSION_NAME"), OUT SessionName);
+	Txt_SessionName->SetText(FText::FromString(SessionName));
+
+	FString MapName;
+	Result.OnlineResult.Session.SessionSettings.Get(FName("MAP_NAME"), OUT MapName);
+	Txt_MapName->SetText(FText::FromString(MapName));
+	
+	// 방장이 설정한 최대 참가자수 - 현재 남은 자리 = 현재 플레이어 수
+	const int32 CurrentPlayers = Result.OnlineResult.Session.SessionSettings.NumPublicConnections - Result.OnlineResult.Session.NumOpenPublicConnections;
+
+	// 방장이 설정한 최대 참자가수
+	const int32 MaxPlayers = Result.OnlineResult.Session.SessionSettings.NumPublicConnections;
+
+	FString PlayerCountString = FString::Printf(TEXT("( %d / %d )"), CurrentPlayers, MaxPlayers);
+	Txt_PlayerCount->SetText(FText::FromString(PlayerCountString));
 }
